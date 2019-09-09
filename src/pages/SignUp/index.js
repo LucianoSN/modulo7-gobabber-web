fd7c3dec129514684d82161b58d3e9297b713b0d
Luciano de Souza Nery <lsnery@gmail.com>
2019-09-09 17:06:52 -0300

    estilos da autenticação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 const SignUp = () => {
-	return <h1>SignUp</h1>;
+	return (
+		<>
+			<img src={logo} alt="GoBarber" />
+
+			<form>
+				<input type="text" placeholder="Nome completo" />
+				<input type="email" placeholder="Seu e-mail" />
+				<input type="password" placeholder="Sua senha secreta" />
+
+				<button type="submit">Criar conta</button>
+				<Link to="/">Já tenho login</Link>
+			</form>
+		</>
+	);
 };
 
 export default SignUp;
