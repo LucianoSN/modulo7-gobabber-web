d021d53b1a8e85995aceb58c0a632b36b5b3b939
Luciano de Souza Nery <lsnery@gmail.com>
2019-09-09 18:42:51 -0300

    configurando store

---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+	token: 123,
+};
+
+const auth = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default auth;
